d22cef63e7d0c3952d207d937ecd291e75a25475
Silentia <yamina1105@email.com>
2025-02-14 10:16:22 +0300

    CollegeCommit

---
+++ DVDyyy/PageApp/AuthoPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DVDyyy.PageApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthoPage.xaml
+    /// </summary>
+    public partial class AuthoPage : Page
+    {
+        public AuthoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClEventAuto(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageApp.AdminMenuPage());
+        }
+    }
+}
